050f42452ea04f04aae41051d8bfe344b9ff4795
Do-young Seo <nalutbae@gmail.com>
2026-01-04 15:19:32 +0900

    feat: add route for command to appropriate utiltiy modules

--- devknife/core/__init__.py
+++ devknife/core/__init__.py
@@ -5,6 +5,14 @@ Core module containing interfaces, data models, and base classes.
 from .interfaces import UtilityModule
 from .models import Command, InputData, ProcessingResult, Config, InputSource
 from .io_handler import InputHandler, OutputFormatter, ErrorHandler, OutputFormat
+from .router import (
+    CommandRegistry, 
+    CommandRouter, 
+    get_global_registry, 
+    get_global_router,
+    register_utility,
+    discover_utilities
+)
 
 __all__ = [
     "UtilityModule",
@@ -17,4 +25,10 @@ __all__ = [
     "OutputFormatter", 
     "ErrorHandler",
     "OutputFormat",
+    "CommandRegistry",
+    "CommandRouter",
+    "get_global_registry",
+    "get_global_router", 
+    "register_utility",
+    "discover_utilities",
 ]
--- devknife/utils/__init__.py
+++ devknife/utils/__init__.py
@@ -1,3 +1,9 @@
 """
 Utility modules package containing all the specific utility implementations.
 """
+
+from .example_utility import ExampleUtility
+
+__all__ = [
+    "ExampleUtility",
+]
